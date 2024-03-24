730c3d51713c42b6482e0b027c2d6ad1646daa1f
Ada Lundhe <ada@datavant.com>
2024-03-24 16:18:06 -0500

    AL: numerous graphics enhancements

--- src/components/mdx/ModeHeader.tsx
+++ src/components/mdx/ModeHeader.tsx
@@ -19,7 +19,7 @@ export const ModeHeader = ({
     )
     
     return (
-        <div className={`font-sans text-[4.5vmin] ${mode === 'light' ? 'text-[#212121]' : 'text-[#F5F5F5]'}`}>
+        <div className={`text-[4.5vmin] ${mode === 'light' ? 'text-[#212121]' : 'text-[#F5F5F5]'}`}>
             {children}
         </div>
     )
